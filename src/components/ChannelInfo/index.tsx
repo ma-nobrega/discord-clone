753cbb1189857dce4011d416039ac185ceb6457b
Matheus Nobrega <matheus.adao.nobrega@gmail.com>
2021-02-11 20:20:50 -0300

    refactor: updating information channel design

--- src/components/ChannelInfo/index.tsx
+++ src/components/ChannelInfo/index.tsx
@@ -2,19 +2,41 @@ import React from 'react';
 
 import {
   Container,
+  Descriptions,
   HashtagIcon,
   Title,
   Separator,
   Description,
+  Options,
+  Notification,
+  PinnedMessages,
+  Members,
+  Input,
+  InputIcon,
+  Inbox,
+  Help,
 } from './styles';
 
 const ChannelInfo: React.FC = () => {
   return (
     <Container>
-      <HashtagIcon />
-      <Title>chat-livre</Title>
-      <Separator />
-      <Description>Canal aberto para conversas</Description>
+      <Descriptions>
+        <HashtagIcon />
+        <Title>chat-livre</Title>
+        <Separator />
+        <Description>Canal aberto para conversas</Description>
+      </Descriptions>
+      <Options>
+        <Notification />
+        <PinnedMessages />
+        <Members />
+        <Input>
+          <input type="text" placeholder="Buscar" />
+          <InputIcon />
+        </Input>
+        <Inbox />
+        <Help />
+      </Options>
     </Container>
   );
 };
--- src/components/ChannelInfo/styles.ts
+++ src/components/ChannelInfo/styles.ts
@@ -1,11 +1,20 @@
 import styled from 'styled-components';
-import { FiHash } from 'react-icons/fi';
+import {
+  FiHash,
+  FiUsers,
+  FiBell,
+  FiHelpCircle,
+  FiInbox,
+  FiSearch,
+} from 'react-icons/fi';
+import { RiPushpinLine } from 'react-icons/ri';
 
 export const Container = styled.div`
   grid-area: CI;
 
   display: flex;
   align-items: center;
+  justify-content: space-between;
 
   padding: 0 17px;
   background-color: var(--primary);
@@ -13,6 +22,12 @@ export const Container = styled.div`
   box-shadow: rgba(0, 0, 0, 0.2) 0px 1px 0px 0px;
   z-index: 2;
 `;
+
+export const Descriptions = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
 export const HashtagIcon = styled(FiHash)`
   width: 24px;
   height: 24px;
@@ -42,4 +57,103 @@ export const Separator = styled.div`
 export const Description = styled.span`
   font-size: 15px;
   color: var(--gray);
+`;
+
+export const Options = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const Notification = styled(FiBell)`
+  width: 24px;
+  height: 24px;
+  color: var(--symbol);
+  cursor: pointer;
+
+  &:hover,
+  &.active {
+    color: var(--white);
+  }
+`;
+
+export const PinnedMessages = styled(RiPushpinLine)`
+  width: 24px;
+  height: 24px;
+  margin-left: 14px;
+
+  color: var(--symbol);
+  cursor: pointer;
+
+  &:hover,
+  &.active {
+    color: var(--white);
+  }
+`;
+
+export const Members = styled(FiUsers)`
+  width: 24px;
+  height: 24px;
+  margin-left: 14px;
+
+  color: var(--symbol);
+  cursor: pointer;
+
+  &:hover,
+  &.active {
+    color: var(--white);
+  }
+`;
+
+export const Input = styled.div`
+  margin-left: 14px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: var(--tertiary);
+  padding: 4px 6px;
+  border-radius: 4px;
+  input {
+    width: 106px;
+    background-color: transparent;
+    color: var(--white);
+    &::placeholder {
+      color: var(--gray);
+    }
+  }
+`;
+
+export const InputIcon = styled(FiSearch)`
+  width: 16px;
+  height: 16px;
+  margin-left: 4px;
+
+  color: var(--symbol);
+`;
+
+export const Inbox = styled(FiInbox)`
+  width: 24px;
+  height: 24px;
+  margin-left: 14px;
+
+  color: var(--symbol);
+  cursor: pointer;
+
+  &:hover,
+  &.active {
+    color: var(--white);
+  }
+`;
+
+export const Help = styled(FiHelpCircle)`
+  width: 24px;
+  height: 24px;
+  margin-left: 14px;
+
+  color: var(--symbol);
+  cursor: pointer;
+
+  &:hover,
+  &.active {
+    color: var(--white);
+  }
 `;
